26071e0f775580b2ca8aef26ba6b28146d47c464
kyohhh <khnm0322@gmail.com>
2021-06-26 01:49:59 +0900

    SCSS_main_おすすめプラン

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.rec-slick').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,//オートプレイON
+    speed: 1000,
+    autoplaySpeed: 5000,//オートプレイスピード5秒
+    arrows: false,//ボタン非表示
+    centerPadding: '100px',//横切れ端の見え方
+    centerMode: true
+  });
+});
