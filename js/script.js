ff77b5fafec39ec9874592a8ab3715be4a6ad165
kyohhh <khnm0322@gmail.com>
2021-07-02 00:38:49 +0900

    レスポンシブ対応_おすすめのプランの作成

--- js/script.js
+++ js/script.js
@@ -9,10 +9,22 @@ $(function () {
     autoplaySpeed: 5000,//オートプレイスピード5秒
     arrows: false,//ボタン非表示
     centerPadding: '100px',//横切れ端の見え方
-    centerMode: true
+    centerMode: true,
+    responsive: [//レスポンシブ対応
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: 0,//横切れ端の見え方
+          centerMode: true
+        }
+      }
+    ]
   });
 });
 
+//ナビ
 $(function () {
   $('#nav-btn').on('click', function () {//nav-btnClassをクリックしたら
     $(this).toggleClass('-active');//-activeClassを付け外しする
