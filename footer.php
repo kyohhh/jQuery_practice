ec7468f08b9c4b877f7fec608adeff5649b6ac8a
kyohhh <khnm0322@gmail.com>
2021-07-14 23:13:11 +0900

    footer.php作成

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- フッター -->
+<footer class="footer">
+        <div class="inner wow animate_animated animate_fadeIn">
+            <nav class="nav-wrap">
+                <ul class="nav-list">
+                    <li class="nav-item"><a href="#">利用規約</a></li>
+                    <li class="nav-item"><a href="#">プライバシーポリシー</a></li>
+                    <li class="nav-item"><a href="#">個人商取引法に基づく表記</a></li>
+                    <li class="nav-item"><a href="#">お問い合わせ</a></li>
+                </ul>
+                <div class="footer-logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/image/logo.png" alt="極楽亭">
+                </div>
+                <p class="copyright">Copyright (c) 2021 <?php bloginfo( 'name' );//ブログ名を表示 ?> All Rights Reserved.</p>
+            </nav>
+        </div>
+    </footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -227,23 +227,4 @@
         </section>
     </main>
 
-    <!-- フッター -->
-    <footer class="footer">
-        <div class="inner wow animate_animated animate_fadeIn">
-            <nav class="nav-wrap">
-                <ul class="nav-list">
-                    <li class="nav-item"><a href="#">利用規約</a></li>
-                    <li class="nav-item"><a href="#">プライバシーポリシー</a></li>
-                    <li class="nav-item"><a href="#">個人商取引法に基づく表記</a></li>
-                    <li class="nav-item"><a href="#">お問い合わせ</a></li>
-                </ul>
-                <div class="footer-logo">
-                    <img src="./image/logo.png" alt="極楽亭">
-                </div>
-                <p class="copyright">Copyright (c) 2021 極楽亭 All Rights Reserved.</p>
-            </nav>
-        </div>
-    </footer>
-</body>
-
-</html>
+<?php get_footer();//footer.phpを取得 PHPで終了するので閉じタグは不要
